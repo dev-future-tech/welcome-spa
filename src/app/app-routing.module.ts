d81236a2cb438db79cb2e5a09b1b0307e591acf6
Anthony Ikeda <anthony.ikeda@gmail.com>
2023-10-08 14:00:30 -0700

    Reorganized the code to be a bit cleaner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,8 @@ import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
